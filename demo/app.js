d87b37bf1d6e3566759cf86820c4ca3225c1ce5a
Jin <305648262@qq.com>
2019-02-01 17:37:12 +0800

    调试

--- demo/app.js
+++ demo/app.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
-// const lumos = require('../../src/data/isEmpty')
-const lumos = {a: function(){}}
+
 
 
 app.set('view engine', 'ejs')
-
-app.locals.lumos = lumos;
+app.use(express.static("./public"));
 
 app.get('/', (req, res) => res.send('Hello World'))
+
 app.get('/cookie', (req, res) => {
-    res.render('cookie', { title: 'Cookie'}); 
+    res.render('cookie', { title: 'Cookie' });
 })
 
 app.listen(3333, () => console.log('Example app listening on port 3333!'))
