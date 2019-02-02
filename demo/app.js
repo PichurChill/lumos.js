e15dbc9dd90a9aaad21ae4a8851787dc2a43f411
Jin <305648262@qq.com>
2019-02-02 10:54:47 +0800

    add travis

--- demo/app.js
+++ demo/app.js
@@ -7,10 +7,16 @@ const ejs = require('ejs')
 app.set('view engine', 'ejs')
 app.use(express.static("./public"));
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => {
+    res.render('index');
+})
 
 app.get('/cookie', (req, res) => {
-    res.render('cookie', { title: 'Cookie' });
+    res.render('cookie', { title: 'cookie' });
+})
+
+app.get('/data', (req, res) => {
+    res.render('data', { title: 'data' });
 })
 
 app.listen(3333, () => console.log('Example app listening on port 3333!'))
