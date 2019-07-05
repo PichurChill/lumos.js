a9cc4208792269bec0ec401ffe1ee254b363ac14
jin <305648262@qq.com>
2019-07-05 22:06:04 +0800

    完善

---
+++ page/script/build.js
@@ -0,0 +1,21 @@
+const rm = require('rimraf')
+const path = require('path')
+const rootPath = path.resolve(__dirname, '../..')
+const fileFn  = require('./fileFn')
+
+// delete old
+rm.sync(path.resolve(rootPath, 'css'))
+rm.sync(path.resolve(rootPath, 'js'))
+rm.sync(path.resolve(rootPath, 'img'))
+rm.sync(path.resolve(rootPath, 'favicon.ico'))
+rm.sync(path.resolve(rootPath, 'index.html'))
+
+
+fileFn.copyDir(path.resolve(rootPath, 'page/dist'), rootPath)
+fileFn.rmdir(path.resolve(rootPath, 'page/dist'))
+
+
+
+
+
+
