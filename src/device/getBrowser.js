10ea126674b335ff52742299f624a888110ac5b4
Jin <305648262@qq.com>
2023-02-13 11:14:32 +0800

    Update getBrowser.js

--- src/device/getBrowser.js
+++ src/device/getBrowser.js
@@ -8,6 +8,7 @@ function getBrowser() {
         s;
         (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? sys.ie = s[1]:
         (s = ua.match(/msie ([\d\.]+)/)) ? sys.ie = s[1] :
+        (s = ua.match(/edg\/([\d\.]+)/)) ? sys.edg = s[1] :
         (s = ua.match(/edge\/([\d\.]+)/)) ? sys.edge = s[1] :
         (s = ua.match(/firefox\/([\d\.]+)/)) ? sys.firefox = s[1] :
         (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? sys.opera = s[1] :
@@ -15,6 +16,7 @@ function getBrowser() {
         (s = ua.match(/version\/([\d\.]+).*safari/)) ? sys.safari = s[1] : 0;
     // 根据关系进行判断
     if (sys.ie) return ({ name: 'ie', version:  sys.ie})
+    if (sys.edg) return ({ name: 'edge', version:  sys.edg})
     if (sys.edge) return ({ name: 'edge', version:  sys.edge})
     if (sys.firefox) return ({ name: 'firefox', version:  sys.firefox})
     if (sys.chrome) return ({ name: 'chrome', version:  sys.chrome})
